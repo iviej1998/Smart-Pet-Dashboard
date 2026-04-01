16f6cd7a6168307d91a2fa5e7939489185b95b0e
Jillian Ivie <iviej@my.erau.edu>
2026-04-01 10:31:45 -0700

    Add PetOwner child class

---
+++ PetOwner.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include "User.h"
+using namespace std;
+
+/********************************************************
+ * Authors: Jillian Ivie,
+ * Version: 1.1
+ * Purpose: Represents a standard system user responsible 
+    for managing pet profiles and interacting with associated devices.
+    This class extends the User class by functionality specific to pet ownership,
+    including pet management, device control, and monitoring pet-related activity.
+ *******************************************************/
+
+class PetOwner : public User {
+protected:
+
+
+};
