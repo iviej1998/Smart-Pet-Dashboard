e99ad605ce80298d6d302af79cdb93b0b38a161c
Jillian Ivie <iviej@my.erau.edu>
2026-04-02 15:50:59 -0700

    Add SmartFeeder.h child class

---
+++ SmartFeeder.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include "Device.h"
+using namespace std;
+
+/********************************************************
+ * Authors: Jillian Ivie,
+ * Version: 1.1
+ * Purpose: Represents the automatic pet feeding device 
+    within the Smart Pet Dashboard system. This class extends
+    the device class and provides functionality for dispensing
+    food and treats, managing feeding schedules, controlling portion sizes,
+    and reporting feeder status. It supports both manual and automated
+    feeding operations to ensure consistent pet care.
+ *******************************************************/
+
+class SmartLight : public Device {
+
+  /* dispense food manually upon request
+    dispense treats manually upon request
+    maintain and execute feeding schedules
+    Manage and store portion size configurations
+    track & report feeder status (FULL, EMPTY, NEAR_EMPTY)
+    record feeding evetns for system logging
+    recieve commands from the system and send acknowledgements back to the dashboard */
+};
