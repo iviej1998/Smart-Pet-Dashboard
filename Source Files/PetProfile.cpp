d696111204af3d208d3b8caec7463bfbbd8571dd
iviej1998 <iviej@my.erau.edu>
2026-04-08 16:26:16 -0700

    Add files via upload

---
+++ Source Files/PetProfile.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Angel Madrigal on 4/6/26.
+//
+#pragma once
+#include "PetProfile.cpp"
+using namespace std;
+
+int PetProfile::getID() { return petID; }
+
+void PetProfile::setName(string name) { petName = name; }
+
+void setType(string type) { petType = type; }
